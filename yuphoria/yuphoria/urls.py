254babdc4c4882df44bc26def85e92c316e30dfd
Harsha-vrdn <121941692+Harsha-vrdn@users.noreply.github.com>
2024-02-03 09:39:14 +0530

    Login page front end

--- yuphoria/yuphoria/urls.py
+++ yuphoria/yuphoria/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('contact/', include('contact.urls')),
     path("user_login/", include("user_login.urls")),
+    path("register", include("user_login.urls")),
     path("auth/", include("django.contrib.auth.urls")),
     path("cart/", include("cart.urls")),
     path("makeit/", include("main.urls")),
